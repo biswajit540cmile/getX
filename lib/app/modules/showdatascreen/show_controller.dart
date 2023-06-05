7daf1ad1bdfefd255306e3ff6e3f840ee4f5d2c6
biswajit540cmile <biswajit.samal@cmile.in>
2023-06-05 15:34:42 +0530

    Pagination 1

--- lib/app/modules/showdatascreen/show_controller.dart
+++ lib/app/modules/showdatascreen/show_controller.dart
@@ -1,28 +1,96 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get/get_connect/sockets/src/socket_notifier.dart';
 import '../../model/get_response.dart';
 
 class ShowController extends GetxController {
   final GetConnect connect = Get.find<GetConnect>();
+ //final ScrollController scrollController = ScrollController();
   GetResponse? getResponse;
   List<GetResponse> res1 = <GetResponse>[].obs;
   var isLoading = false.obs;//use for
+  int page = 1;
+  var hasNextPage = true.obs;
+  var isFirstLoadRunning = false.obs;
+  var isLoadMoreRunning = false.obs;
 
+  Future<void> getDataF() async {
+
+    isFirstLoadRunning.value = true;
+    try{
+      Response response =
+      await connect.get('https://jsonplaceholder.typicode.com/albums?_page=$page&_limit=10');
+      if (response.statusCode == 200) {
+        var res = response.body as Iterable;
+        res1 = res.map((v) => GetResponse.fromJson(v)).toList();
+      //  isLoading.value = true;
+       // page++;
+      } else {
+        print(response.statusCode);
+      }
+    }catch(e){
+      print(e);
+    } finally {
+      isFirstLoadRunning.value = false;
+    }
+
+  }
+
+  void loadMore() async {
+    print("Extent ${scrollController.position.maxScrollExtent}");
+    print("Pixels ${scrollController.position.pixels}");
+    if (hasNextPage.value == true &&
+        isFirstLoadRunning.value == false &&
+        isLoadMoreRunning.value == false &&
+        scrollController.position.maxScrollExtent <= 118)
+    {
+if(scrollController.position.pixels == scrollController.position.maxScrollExtent){
+        isLoadMoreRunning.value = true; // Display a progress indicator at the bottom
+        page += 1; // Increase _page by 1
+
+        try{
+          Response response =
+          await connect.get('https://jsonplaceholder.typicode.com/albums?_page=$page&_limit=10');
+          //?_page=$page
+          //  print("@@  ${response.statusCode}");
+          if (response.statusCode == 200) {
+            var res = response.body as Iterable;
+
+            if (res.isNotEmpty) {
+              res1 = res.map((v) => GetResponse.fromJson(v)).toList();
+              isLoading.value = true;
+              page++;
+
+            } else {
+              // This means there is no more data
+              // and therefore, we will not send another GET request
+              hasNextPage.value = false;
+            }
+          } else {
+            print(response.statusCode);
+          }
+        }catch(e){
+          print(e);
+        } finally {
+          // isLoading.value = false;
+        }
+        isLoadMoreRunning.value = false;
+    }
+
+  }}
+  late ScrollController scrollController;
   @override
   void onInit() {
     super.onInit();
-    getDataMethod();
+    getDataF();
+scrollController =  ScrollController()..addListener(loadMore);
   }
 
-  Future<void> getDataMethod() async {
-    Response response =
-        await connect.get('https://jsonplaceholder.typicode.com/albums');
-    //print("@@  ${response.statusCode}");
-    if (response.statusCode == 200) {
-      var res = response.body as Iterable;
-      res1 = res.map((v) => GetResponse.fromJson(v)).toList();
-      isLoading.value = true;
-    } else {
-      return;
-    }
+  @override
+  void onClose() {
+    scrollController.removeListener(loadMore);
+    super.onClose();
   }
+
+
 }
--- lib/app/modules/showdatascreen/showdata_screen.dart
+++ lib/app/modules/showdatascreen/showdata_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:get/get.dart';
 import 'package:getx/app/modules/showdatascreen/show_controller.dart';
 class ShowDataScreen extends StatelessWidget {
@@ -11,12 +12,14 @@ class ShowDataScreen extends StatelessWidget {
           centerTitle: true,
           title: const Text('GetX Data Screen'),
         ),
-        body:   Obx(()=>!controller.isLoading.value ? const Center(child: CircularProgressIndicator()) :
+        body:   Obx(()=> controller.isFirstLoadRunning.value ? const Center(child: CircularProgressIndicator()) :
           SingleChildScrollView(
+            controller: controller.scrollController,
             child: Column(
               children: [
                 const Text("Api Calling Screen"),
      ListView.builder(
+         // controller: scrollController,
           shrinkWrap: true,
           physics: const BouncingScrollPhysics(),
           itemCount: controller.res1.length,
@@ -24,6 +27,7 @@ class ShowDataScreen extends StatelessWidget {
             return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
+              height: 55,
               decoration: BoxDecoration(
                 borderRadius: const BorderRadius.all(Radius.circular(10)),
                 border: Border.all(width: 2)
@@ -37,7 +41,26 @@ class ShowDataScreen extends StatelessWidget {
               ),
             ),
           );
-      },)
+      },),
+                // when the _loadMore function is running
+                if (controller.isLoadMoreRunning.value == true)
+                  const Padding(
+                    padding: EdgeInsets.only(top: 10, bottom: 40),
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  ),
+
+                // When nothing else to load
+                if (controller.hasNextPage.value == false)
+                  Container(
+                    padding: const EdgeInsets.only(top: 30, bottom: 40),
+                    color: Colors.amber,
+                    child: const Center(
+                      child: Text('You have fetched all of the content'),
+                    ),
+                  ),
+
               ]
             ),
           ),
